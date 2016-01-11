4358727684d25ef27dc7bab719b4549465a44ede
nfichter <nfichter@stuy.edu>
2016-01-11 17:16:43 -0500

    Added a color variable to change the color of the border (must be in HEX FORMAT)

--- Border.java
+++ Border.java
@@ -2,20 +2,24 @@ import java.util.*;
 import java.awt.image.*;
 
 public class Border extends ImageDN {
-    private ArrayList<Integer> borderPos;
+	private ArrayList<Integer> borderPos;
+	private String color;
 
-    public Border(BufferedImage brdr) {
-	super(brdr);
-	borderPos = new ArrayList<Integer>();
-	for (int row = 0; row < getHexPistols().length; row++) {
-	    for (int col = 0; col < getHexPistols().length; col++) {
-		if (getHexPistols()[row][col].equals("#000000")) {
-		    borderPos.add(row);
-		    borderPos.add(col);
+	public Border(BufferedImage brdr, String color) {
+		super(brdr);
+		this.color = color;
+		borderPos = new ArrayList<Integer>();
+		for (int row = 0; row < getHexPistols().length; row++) {
+			for (int col = 0; col < getHexPistols().length; col++) {
+				if (getHexPistols()[row][col].equals("#000000")) {
+					borderPos.add(row);
+					borderPos.add(col);
+				}
+			}
 		}
-	    }
 	}
-    }
-
-    
+	
+	public ArrayList<Integer> getBorderPos() {
+		return borderPos();
+	}
 }
