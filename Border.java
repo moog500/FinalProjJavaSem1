91d5f9dc3f1bf5298fa41553dc85aa0a7d240863
Noah Fichter <nfichter@stuy.edu>
2016-01-14 13:10:40 -0500

    Set back to original

--- Border.java
+++ Border.java
@@ -2,24 +2,28 @@ import java.util.*;
 import java.awt.image.*;
 
 public class Border extends ImageDN {
-	private ArrayList<Integer> borderPos;
-	private String color;
+    private ArrayList<Integer> borderPos;
+    private String color;
 
-	public Border(BufferedImage brdr, String color) {
-		super(brdr);
-		this.color = color;
-		borderPos = new ArrayList<Integer>();
-		for (int row = 0; row < getHexPistols().length; row++) {
-			for (int col = 0; col < getHexPistols().length; col++) {
-				if (getHexPistols()[row][col].equals("#000000")) {
-					borderPos.add(row);
-					borderPos.add(col);
-				}
-			}
+    public Border(BufferedImage brdr, String color) {
+	super(brdr);
+	this.color = color;
+	borderPos = new ArrayList<Integer>();
+	for (int row = 0; row < getPixels().length; row++) {
+	    for (int col = 0; col < getPixels()[row].length; col++) {
+		if (getPixels()[row][col].equals("000000000")) {
+		    borderPos.add(row);
+		    borderPos.add(col);
 		}
+	    }
 	}
+    }
 	
-	public ArrayList<Integer> getBorderPos() {
-		return borderPos();
-	}
+    public ArrayList<Integer> getBorderPos() {
+	return borderPos;
+    }
+
+    public String getColor() {
+	return color;
+    }
 }
