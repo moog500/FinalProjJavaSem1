39208413909cd02e2c6d3dfa9008fa79f5763b3d
nfichter <nfichter@stuy.edu>
2016-01-18 15:04:28 -0500

    Began driver - see readme for syntax, work from there

--- Driver.java
+++ Driver.java
@@ -1,10 +1,19 @@
 public class Driver {
 	public static void main(String[] args) {
-		if (args.length <= 1) {
-			System.out.println("Please read the README.txt for instructions.");
-		}
-		else if (args.length == 2) {
+		//Scaling
+		if (args[1].equals("s")) {
 			
+		}
+		//Borders
+		else if (args[1].equals("b")) {
+			
+		}
+		//Moods
+		else if (args[1].equals("m")) {
+			
+		}
+		else {
+			System.out.println("Please read the README.txt for instructions!");
 		}
 	}
 }
