06f98e86210a41f08846827c70ea8b3069793dd2
nfichter <nfichter@stuy.edu>
2016-01-18 17:35:07 -0500

    Added Driver functionality for scaling

--- Driver.java
+++ Driver.java
@@ -1,16 +1,35 @@
 public class Driver {
 	public static void main(String[] args) {
+		BufferedImage bIn = null;
 		//Scaling
 		if (args[1].equals("s")) {
-			
+			try {
+				bIn = ImageIO.read(new File(args[0]));
+				ImageDN in = new ImageDN(b);
+				ImageDN out = in.scale1(args[2],args[3]);
+				out.outputImage(args[4],args[5]);
+			}
+			catch (Exception e) {
+				System.out.println("Please read the README for instructions!");
+			}
 		}
 		//Borders
 		else if (args[1].equals("b")) {
-			
+			try {
+				
+			}
+			catch (Exception e) {
+				System.out.println("Please read the README for instructions!");
+			}
 		}
 		//Moods
 		else if (args[1].equals("m")) {
-			
+			try {
+				
+			}
+			catch (Exception e) {
+				System.out.println("Please read the README for instructions!");
+			}
 		}
 		else {
 			System.out.println("Please read the README.txt for instructions!");
